b4a058bc84b0751ce58c11def30b0fc917d95825
Mohd. Ahmed Khan <mohd.ahmedkhan190@gmail.com>
2026-09-19 12:57:02 +0530

    expressions

---
+++ app/expression/page.tsx
@@ -0,0 +1,11 @@
+export default function Car() {
+    return (
+        <>
+            <div>
+                <main className="flex items-center justify-center h-screen">
+                    <p>2 + 2 = {2 + 2}</p>
+                </main>
+            </div>
+        </>
+    );
+}
