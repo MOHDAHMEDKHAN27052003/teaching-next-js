df6592aa5031118d35d53c149d1042b4c919ff0e
Mohd. Ahmed Khan <mohd.ahmedkhan190@gmail.com>
2026-09-22 13:16:13 +0530

    submit form

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, SubmitEvent, useState } from "react"
 
 export default function Form() {
     const [name, setName] = useState("");
@@ -9,16 +9,33 @@ export default function Form() {
         setName(e.target.value);
     }
 
+    function handleSubmit(e: SubmitEvent<HTMLFormElement>) {
+        e.preventDefault();
+        alert(name);
+    }
+
     return (
         <>
             <div>
-                <main className="flex items-center justify-center h-screen">
-                    <form>
-                        <label>
-                            Enter your name:
-                            <input type="text" value={name} onChange={handleChange} />
-                        </label>
-                        <p>{name}</p>
+                <main className="flex items-center justify-center min-h-screen">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="space-y-6"
+                    >
+                        <label htmlFor="name">Enter your name</label>
+                        <input
+                            id="name"
+                            type="text"
+                            value={name}
+                            onChange={handleChange}
+                            className="w-full rounded-lg border px-4 py-2.5"
+                        />
+
+                        <button
+                            className="w-full rounded-lg border px-4 py-2.5 bg-green-400 text-white"
+                        >
+                            Submit
+                        </button>
                     </form>
                 </main>
             </div>
